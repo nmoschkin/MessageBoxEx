7c7c011571943f0751631fea1d357b2bf7dad198
Nathan Moschkin <iroywrit@gmail.com>
2020-08-15 15:11:35 -0400

    More change to the example program.

--- TestApp/Form1.cs
+++ TestApp/Form1.cs
@@ -119,7 +119,9 @@ namespace TestApp
             sb.AppendLine("");
             sb.AppendLine("* Drop Down Menus");
             sb.AppendLine("");
-            sb.AppendLine("Accomodation for Messages Of lengthy height, and lengthy width, although we trust people to try to be brief, long messages are possible.");
+            sb.AppendLine("* Accomodation for Messages of large height, and width. (Although, we trust people to try to be brief as possible.)");
+            sb.AppendLine("");
+            sb.AppendLine("Copyright (C) Nathaniel Moschkin.  Licensed under the MIT license.");
 
             var cfg = new MessageBoxExConfig()
             {
