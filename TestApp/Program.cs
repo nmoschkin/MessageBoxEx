29448eb948638f3583eb41b6df0fe8d00e1fc918
Nathan Moschkin <iroywrit@gmail.com>
2020-08-15 16:02:22 -0400

    Created an enable visual styles prompt.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataTools.MessageBoxEx;
 
 namespace TestApp
 {
@@ -12,10 +13,39 @@ namespace TestApp
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            bool vs = false;
+
+            if (args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    if (arg.ToLower() == "/vs")
+                    {
+                        vs = true;
+                        
+                    }
+                }
+            }
+
+            if (!vs)
+            {
+                var res = MessageBoxEx.Show("Enable Visual Styles?", "Visual Styles", MessageBoxExButtonSet.YesNo, MessageBoxExIcons.Question);
+                
+                if (res == MessageBoxExResult.Yes)
+                {
+                    System.Diagnostics.Process.Start(Application.ExecutablePath, "/vs");
+                    Application.Exit();
+
+                    return;
+                }
+            }
+
+            if (vs) Application.EnableVisualStyles();
+
             Application.Run(new Form1());
         }
     }
